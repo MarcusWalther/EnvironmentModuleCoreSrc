e995b8aa08627004fc40cee0ff2a69eb2c8fba9b
Marcus Walther <walther72@hotmail.de>
2020-06-23 20:18:07 +0200

    * Removed nuspec file, properties are now handled in csproj file directly * Added pack step to pipeline description

--- Tasks.build.ps1
+++ Tasks.build.ps1
@@ -1,7 +1,19 @@
 param(
-    $Configuration = 'Release'
+    $Configuration = 'Release',
+    $Suffix = ''
 )
 
 task Build {
-    dotnet build (Join-Path "$PSScriptRoot" "EnvironmentModuleCore.sln") -c "$Configuration"
+    dotnet build (Join-Path "$PSScriptRoot" "EnvironmentModuleCore.sln") -c "$Configuration" --version-suffix "$Suffix"
+}
+
+task Pack {
+    $projectFile = $(Join-Path "$PSScriptRoot" (Join-Path "EnvironmentModuleCore" "EnvironmentModuleCore.csproj"))
+
+    if([string]::IsNullOrEmpty($Suffix)) {
+        dotnet pack $projectFile --no-build -c "$Configuration" -o "Package"
+    }
+    else {
+        dotnet pack $projectFile --no-build -c "$Configuration" -o "Package" --version-suffix "$Suffix"
+    }
 }
