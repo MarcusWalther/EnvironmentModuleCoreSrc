4349bffc8f6e5bc60d8e87ab2f1fad3b8d506745
Marcus Walther <walther72@hotmail.de>
2020-06-23 20:48:32 +0200

    * Added deploy step to pipeline

--- Tasks.build.ps1
+++ Tasks.build.ps1
@@ -1,13 +1,23 @@
 param(
     $Configuration = 'Release',
-    $Suffix = ''
+    $Suffix = $null,
+    $NugetSource = $null,
+    $NugetApiKey = $null
 )
 
-task Build {
+task Prepare {
+    dotnet restore --interactive
+}
+
+task Build Prepare, {
     dotnet build (Join-Path "$PSScriptRoot" "EnvironmentModuleCore.sln") -c "$Configuration" --version-suffix "$Suffix"
 }
 
 task Pack {
+    if(Test-Path "Package") {
+        Remove-Item "Package" -Recurse
+    }
+
     $projectFile = $(Join-Path "$PSScriptRoot" (Join-Path "EnvironmentModuleCore" "EnvironmentModuleCore.csproj"))
 
     if([string]::IsNullOrEmpty($Suffix)) {
@@ -16,4 +26,8 @@ task Pack {
     else {
         dotnet pack $projectFile --no-build -c "$Configuration" -o "Package" --version-suffix "$Suffix"
     }
+}
+
+task Deploy {
+    dotnet nuget push --source "$NugetSource" --api-key "$NugetApiKey" "Package\*.nupkg"
 }
